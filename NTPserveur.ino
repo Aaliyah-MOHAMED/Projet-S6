574eaafd845d5e09938ae0909438d127e047919e
Aaliyah-MOHAMED <mohamed.aaliyah.pro@gmail.com>
2025-05-31 11:46:53 +0200

    NTPserveur.ino

---
+++ NTPserveur.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+#include "time.h"
+
+// Paramètres à modifier
+
+const char* ssid = "  "; // Nom de la box Wi-Fi
+const char* password = "  "; // MDP de la box Wi-Fi
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600 * 1;
+const int   daylightOffset_sec = 3600 * 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA); // Optional
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
+  // On configure le seveur NTP
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+void loop() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  delay(1000);
+}
